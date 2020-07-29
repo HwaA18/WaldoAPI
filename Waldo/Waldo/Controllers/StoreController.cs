10b2899934614a89ae5a3f0dfbea030700e2da2a
Samantha Tang <stang10@terpmail.umd.edu>
2020-07-29 03:17:03 -0400

    Began the controller for Stores, did get, still need post

---
+++ Waldo/Waldo/Controllers/StoreController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Waldo.Service;
+using Waldo.Entity;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Waldo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        StoreService MyService = new StoreService();
+
+        // GET: api/values
+        [EnableCors("TestPolicy")]
+        [HttpGet]
+        public IEnumerable<Store> Get()
+        {
+            return MyService.GetStores();
+        }
+
+        // GET api/values/5
+        [EnableCors("TestPolicy")]
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            Store myStore = MyService.GetStore(id);
+            if (myStore == null)
+            {
+                return "No Store with that Id";
+            }
+            else
+            {
+                return "Name: " + myStore.Name + " Address: " + myStore.Address;
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Waldo/Waldo/Service/StoreService.cs
@@ -0,0 +1,138 @@
+﻿using Waldo.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Waldo.Service
+{
+    public class StoreService
+    {
+
+        public List<Store> GetStores()
+        {
+            List<Store> lsStores = new List<Store>();
+
+            SqlConnection db =
+                new SqlConnection("Server=tcp:waldoserver.database.windows.net,1433;Initial Catalog=waldo;Persist Security Info=False;User ID=waldo;Password=1234@terp;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            db.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Stores");
+            cmd.Connection = db;
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader != null)
+            {
+                Store s = null;
+
+                while (reader.Read())
+                {
+                    s = new Store();
+                    s.Name = reader["Name"].ToString();
+                    s.Latitude = reader["Latitude"].ToString();
+                    s.Longitude = reader["Longitude"].ToString();
+                    s.Address = reader["Address"].ToString();
+                    s.MasksRequired = reader["ReqMasks"].ToString();
+
+                    s.Masks = reader["Masks"].ToString();
+                    s.Gloves = reader["Gloves"].ToString();
+                    s.HandSanitizer = reader["HandSanitizer"].ToString();
+                    s.PaperTowels = reader["PaperTowels"].ToString();
+                    s.ToiletPaper = reader["ToiletPaper"].ToString();
+                    s.LiquidSoap = reader["LiquidSoap"].ToString();
+                    s.BarSoap = reader["BarSoap"].ToString();
+                    s.CleaningWipes = reader["CleaningWipes"].ToString();
+                    s.AerosolDisinfectant = reader["AerosolDisinfectants"].ToString();
+                    s.Bleach = reader["Bleach"].ToString();
+                    s.FlushableWipes = reader["FlushableWipes"].ToString();
+                    s.Tissues = reader["Tissues"].ToString();
+                    s.Diapers = reader["Diapers"].ToString();
+                    s.WaterFilters = reader["WaterFilters"].ToString();
+                    s.ColdRemedies = reader["ColdRemedies"].ToString();
+                    s.CoughRemedies = reader["CoughRemedies"].ToString();
+                    s.RubbingAlcohol = reader["RubbingAlcohol"].ToString();
+                    s.Antiseptic = reader["Antiseptic"].ToString();
+                    s.Thermometer = reader["Thermometer"].ToString();
+                    s.FirstAidKit = reader["FirstAidKit"].ToString();
+                    s.WaterBottles = reader["WaterBottles"].ToString();
+                    s.Eggs = reader["Eggs"].ToString();
+                    s.Milk = reader["Milk"].ToString();
+                    s.Bread = reader["Bread"].ToString();
+                    s.Beef = reader["Beef"].ToString();
+                    s.Chicken = reader["Chicken"].ToString();
+                    s.Pork = reader["Pork"].ToString();
+                    s.Yeast = reader["Yeast"].ToString();
+
+                    lsStores.Add(s);
+                }
+            }
+
+            db.Close();
+
+            return lsStores;
+        }
+
+        public Store GetStore(int StoreId)
+        {
+            SqlConnection db =
+                new SqlConnection("Server=tcp:waldoserver.database.windows.net,1433;Initial Catalog=waldo;Persist Security Info=False;User ID=waldo;Password=1234@terp;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            db.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Stores WHERE StoreId = @StoreId");
+            cmd.Parameters.Add(new SqlParameter { ParameterName = "@StoreId", DbType = System.Data.DbType.Int32, Value = StoreId });
+
+            cmd.Connection = db;
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader != null)
+            {
+                if (reader.Read())
+                {
+                    Store s = new Store();
+                    s.Name = reader["Name"].ToString();
+                    s.Latitude = reader["Latitude"].ToString();
+                    s.Longitude = reader["Longitude"].ToString();
+                    s.Address = reader["Address"].ToString();
+                    s.MasksRequired = reader["ReqMasks"].ToString();
+
+                    s.Masks = reader["Masks"].ToString();
+                    s.Gloves = reader["Gloves"].ToString();
+                    s.HandSanitizer = reader["HandSanitizer"].ToString();
+                    s.PaperTowels = reader["PaperTowels"].ToString();
+                    s.ToiletPaper = reader["ToiletPaper"].ToString();
+                    s.LiquidSoap = reader["LiquidSoap"].ToString();
+                    s.BarSoap = reader["BarSoap"].ToString();
+                    s.CleaningWipes = reader["CleaningWipes"].ToString();
+                    s.AerosolDisinfectant = reader["AerosolDisinfectants"].ToString();
+                    s.Bleach = reader["Bleach"].ToString();
+                    s.FlushableWipes = reader["FlushableWipes"].ToString();
+                    s.Tissues = reader["Tissues"].ToString();
+                    s.Diapers = reader["Diapers"].ToString();
+                    s.WaterFilters = reader["WaterFilters"].ToString();
+                    s.ColdRemedies = reader["ColdRemedies"].ToString();
+                    s.CoughRemedies = reader["CoughRemedies"].ToString();
+                    s.RubbingAlcohol = reader["RubbingAlcohol"].ToString();
+                    s.Antiseptic = reader["Antiseptic"].ToString();
+                    s.Thermometer = reader["Thermometer"].ToString();
+                    s.FirstAidKit = reader["FirstAidKit"].ToString();
+                    s.WaterBottles = reader["WaterBottles"].ToString();
+                    s.Eggs = reader["Eggs"].ToString();
+                    s.Milk = reader["Milk"].ToString();
+                    s.Bread = reader["Bread"].ToString();
+                    s.Beef = reader["Beef"].ToString();
+                    s.Chicken = reader["Chicken"].ToString();
+                    s.Pork = reader["Pork"].ToString();
+                    s.Yeast = reader["Yeast"].ToString();
+
+                    db.Close();
+                    return s;
+                }
+            }
+
+            db.Close();
+
+            return null;
+        }
+    }
+}
